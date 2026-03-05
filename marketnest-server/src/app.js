24c716e8d54473f69a470c0afe9591cde56abb49
Konkipudi Ramesh <161071138+Konkipudi-Ramesh@users.noreply.github.com>
2026-03-05 18:01:48 +0530

    Update CORS configuration to allow additional origin

--- marketnest-server/src/app.js
+++ marketnest-server/src/app.js
@@ -4,16 +4,20 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
 import productRoutes from "./routes/product.routes.js";
 
-
-
 const app = express();
 
 app.use(express.json());
+
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: [
+    "http://localhost:5173",
+    "https://trizen-git-main-rameshs-projects-3580b5a9.vercel.app"
+  ],
   credentials: true
 }));
+
 app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 
